583cb455a45c84b44758ecafda467d0017dcccd0
Naveen Reddy Ramuni <144018898+naveenreddyramuni@users.noreply.github.com>
2026-03-30 18:00:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -777,4 +777,5 @@ conn.close()
 # ---------------- RUN ---------------- #
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
